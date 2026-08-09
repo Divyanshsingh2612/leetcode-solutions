088b3420462006a0f036065704672f8d1a79fefa
Divyansh Singh <thakurshouryasingh0@gmail.com>
2026-08-09 23:16:01 +0530

    Time: 37 ms (96.65%), Space: 29.1 MB (73.6%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,42 +1,36 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>> answer;
-
+        vector<vector<int>> result;
+        int n = nums.size();
         sort(nums.begin(), nums.end());
 
-        for (int i = 0; i < nums.size() - 2; i++) {
+        for (int i = 0; i < n - 2; i++) {
+            // Skip duplicate anchors to avoid duplicate triplets
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
 
-            if (i > 0 && nums[i] == nums[i - 1])
-                continue;
+            // Smallest element is positive -> no triplet can sum to 0
+            if (nums[i] > 0) break;
 
-            int left = i + 1;
-            int right = nums.size() - 1;
+            int left = i + 1, right = n - 1;
+            int target = -nums[i];
 
             while (left < right) {
-                int sum = nums[i] + nums[left] + nums[right];
-
-                if (sum == 0) {
-                    answer.push_back({nums[i], nums[left], nums[right]});
-
-                    while (left < right && nums[left] == nums[left + 1])
-                        left++;
-
-                    while (left < right && nums[right] == nums[right - 1])
-                        right--;
-
+                int sum = nums[left] + nums[right];
+                if (sum == target) {
+                    result.push_back({nums[i], nums[left], nums[right]});
+                    // Skip duplicates on both pointers
+                    while (left < right && nums[left] == nums[left + 1]) left++;
+                    while (left < right && nums[right] == nums[right - 1]) right--;
                     left++;
                     right--;
-                }
-                else if (sum < 0) {
-                    left++;
-                }
-                else {
-                    right--;
+                } else if (sum < target) {
+                    left++;   // need a larger sum
+                } else {
+                    right--;  // need a smaller sum
                 }
             }
         }
-
-        return answer;
+        return result;
     }
 };
